32d385454d1f4a4cb1ce6c8f59df92461f3d22dc
Relyful <13381270+Relyful@users.noreply.github.com>
2024-05-14 23:33:04 +0200

    Create mergeSort function

---
+++ mergeSort.js
@@ -0,0 +1,30 @@
+function mergeSort(array) {
+  if (array.length <= 1) {
+    return array;
+  }
+  else {
+    let half = Math.ceil(array.length / 2);
+    const firstHalfSorted = mergeSort(array.slice(0, half));
+    const secondHalfSorted = mergeSort(array.slice(half));
+    let mergedSortedArr = [];
+    let firstHalfNumber;
+    let secondHalfNumber
+
+    while (firstHalfSorted.length > 0 && secondHalfSorted.length > 0) {
+      firstHalfNumber = firstHalfSorted[0];
+      secondHalfNumber = secondHalfSorted[0];
+      if (firstHalfNumber > secondHalfNumber) {
+        mergedSortedArr.push(secondHalfNumber);
+        secondHalfNumber = secondHalfSorted.shift();
+      } else {
+        mergedSortedArr.push(firstHalfNumber);
+        firstHalfNumber = firstHalfSorted.shift();
+      }
+    }
+    mergedSortedArr.push(...firstHalfSorted);
+    mergedSortedArr.push(...secondHalfSorted);
+    return mergedSortedArr;
+  }
+}
+
+console.log(mergeSort([105, 79, 100, 110]));
